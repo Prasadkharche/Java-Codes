a13569acd1021473e84556b03009c120eacf0ddc
Prasadkharche <pkharche4@gmail.com>
2024-09-30 11:22:08 +0530

    second code commit

---
+++ BeautificationInIntergers.java
@@ -0,0 +1,9 @@
+public class BeautificationInIntergers {
+    public static void main(String[] args) {
+        //we can use underscore to represent number as a part of making number more readable.
+        int Money = 23_45_654;
+        int Money1 = 0b11_10;
+        System.out.println(Money);
+        System.out.println(Money1);
+    }
+}
