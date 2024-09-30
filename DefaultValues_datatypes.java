b8d3e43fb01a51f919357cb7164efc428808286f
Prasadkharche <pkharche4@gmail.com>
2024-09-30 10:52:02 +0530

    First Commit

---
+++ DefaultValues_datatypes.java
@@ -0,0 +1,20 @@
+class DefaultValues_datatypes{
+    static int i;
+    static float f;
+    static char c;
+    static double d;
+    static long l;
+    static short s;
+    static boolean b;
+    public static void main(String[] args) {
+        
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(b);
+       
+    }
+}
